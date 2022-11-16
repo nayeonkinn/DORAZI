67f468597311650677b56ec6ba94115f5fa6a606
Young312886 <hongym0497@gmail.com>
2022-11-16 22:55:34 +0900

    Add: Movies Serailizer creation

--- final-pjt-back/backserver/settings.py
+++ final-pjt-back/backserver/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'articles',
     'movies',
     'moviecollections',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
