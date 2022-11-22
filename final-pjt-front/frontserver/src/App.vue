0d81459a5973af35e3f8f6a69929aaa19d21c69a
hongym0497 <hongym0497@gmail.com>
2022-11-22 16:53:58 +0900

    Feat: 영화 리스트 출력 화면 조절

--- final-pjt-front/frontserver/src/App.vue
+++ final-pjt-front/frontserver/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div class="container3">
     <app-nav/>
     <router-view/>
   </div>
--- final-pjt-front/frontserver/src/views/movies/MovieView.vue
+++ final-pjt-front/frontserver/src/views/movies/MovieView.vue
@@ -1,22 +1,59 @@
 <template>
   <div>
-    <h1>MovieView</h1>
-    <MovieList/>
+    <section class="movies">
+      <h1>MovieView</h1>
+      <div class="movies-grid">
+        <Movie v-for="movie in movielist" :key="movie.id" :movie="movie"/>
+      </div>
+      <button class="load-more">LOAD MORE</button>
+    </section>
   </div>
 </template>
 
 <script>
-import MovieList from '@/components/movies/MovieList.vue';
+import Movie from "@/components/movies/Movie.vue";
+import axios from "axios";
 
+const API_URL = 'http://127.0.0.1:8000'
 export default {
   name:"MovieView",
   components:{
-    MovieList,
+    Movie,
 
+  },
+  data() {
+    return {
+      movielist: []
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+    makelist() {
+      axios({
+        method: 'get',
+        url: `${API_URL}/movies/`,
+        data: {
+
+        }
+      })
+      .then((res) => {
+        // console.log(res);
+        this.movielist = res.data
+      })
+      .catch((err) =>{
+        console.log(err);
+      })
+    }
+  },
+  mounted(){
+    this.makelist()
   }
 }
 </script>
 
 <style>
+  @import '@/assets/main.css';
 
 </style>
