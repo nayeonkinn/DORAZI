55b5330509d202f1b82b3f36688e41ce6d54afd6
김나연 <nayeonkinn@gmail.com>
2022-11-24 23:46:22 +0900

    Fix: 로그인 페이지 이미지 링크 수정

--- final-pjt-front/frontserver/src/views/accounts/LoginView.vue
+++ final-pjt-front/frontserver/src/views/accounts/LoginView.vue
@@ -4,7 +4,7 @@
       v-if="fakeLogo"
       @click="changeLogo"
       class="img mb-5"
-      src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/ff60be88-063a-4ff0-a57f-ccab09f7997c/logo1_wh.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221123%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221123T122435Z&X-Amz-Expires=86400&X-Amz-Signature=d25ccbefa73ad508a2ac003fe9edb607c3396ab14f49ea860de31aedecc1ddd0&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22logo1_wh.png%22&x-id=GetObject"
+      src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/ff60be88-063a-4ff0-a57f-ccab09f7997c/logo1_wh.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221124%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221124T144506Z&X-Amz-Expires=86400&X-Amz-Signature=58a5d018b18d167f7273bd7c69631d541a7034a493f02f7bb6166275041918d1&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22logo1_wh.png%22&x-id=GetObject"
       alt="fake logo"
       style="width: 250px; margin-top: 100px"
       />
@@ -12,7 +12,7 @@
       v-else
       @click="changeLogo"
       class="img mb-5"
-      src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/8fdba63a-7c45-4094-bb51-8a4f5723a6ae/Picture1.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221123%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221123T123549Z&X-Amz-Expires=86400&X-Amz-Signature=31fd3855f09fc521fd15210886d7265ecb64c2ea48589c1dde6035316c0c4f8f&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Picture1.png%22&x-id=GetObject"
+      src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/8fdba63a-7c45-4094-bb51-8a4f5723a6ae/Picture1.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221124%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221124T144539Z&X-Amz-Expires=86400&X-Amz-Signature=490a9e5acd800e2900e9370436e6f858de096af785e5455e738bfa02bb9f477e&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Picture1.png%22&x-id=GetObject"
       alt="real logo (happy developers)"
       style="width: 250px; margin-top: 100px"
     />
