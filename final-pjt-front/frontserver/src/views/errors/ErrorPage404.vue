c1c45a7cee852eb57ef341edb77031302fad5283
김나연 <nayeonkinn@gmail.com>
2022-11-24 23:52:35 +0900

    Fix: 이미지 assets 폴더에 추가

--- final-pjt-front/frontserver/src/views/errors/ErrorPage404.vue
+++ final-pjt-front/frontserver/src/views/errors/ErrorPage404.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div>
       <h1 class="my-5">404 NOT FOUND</h1>
-      <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/362c6343-a8c8-4a32-a9df-c5dabef9e7e3/%E1%84%82%E1%85%A2_%E1%84%91%E1%85%B3%E1%84%85%E1%85%A9%E1%84%8C%E1%85%A6%E1%86%A8%E1%84%90%E1%85%B3.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221124%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221124T113249Z&X-Amz-Expires=86400&X-Amz-Signature=80db03639a66c04dbf4b4210717d381f70e538d181aa0e5827509ecf9a4c962e&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22%25E1%2584%2582%25E1%2585%25A2%2520%25E1%2584%2591%25E1%2585%25B3%25E1%2584%2585%25E1%2585%25A9%25E1%2584%258C%25E1%2585%25A6%25E1%2586%25A8%25E1%2584%2590%25E1%2585%25B3.jpg%22&x-id=GetObject" alt="404 NOT FOUND" style="width: 500px">
+      <img src="@/assets/404.jpg" alt="404 NOT FOUND" style="width: 500px">
     </div>
     </div>
   </template>
