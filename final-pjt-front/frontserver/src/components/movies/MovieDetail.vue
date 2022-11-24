af3ca0860d49cb7693c6bbd914afbbb31b7040df
김나연 <nayeonkinn@gmail.com>
2022-11-25 05:41:26 +0900

    Fix: 404 페이지 라우터 수정

--- final-pjt-front/frontserver/src/components/movies/MovieDetail.vue
+++ final-pjt-front/frontserver/src/components/movies/MovieDetail.vue
@@ -275,6 +275,7 @@ export default {
         })
         .catch((err) => {
           console.log(err);
+          this.$router.push('/404')
         });
     },
   },
--- final-pjt-front/frontserver/src/views/articles/ArticleDetailView.vue
+++ final-pjt-front/frontserver/src/views/articles/ArticleDetailView.vue
@@ -46,6 +46,7 @@ export default {
         })
         .catch((err) => {
           console.log(err);
+          this.$router.push('/404')
         });
     },
   },
